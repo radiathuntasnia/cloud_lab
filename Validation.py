428eae090e9f655c215f54e5f826329dd51bdbb4
radiathuntasnia <82764016+radiathuntasnia@users.noreply.github.com>
2023-07-26 00:40:11 +0600

    Update Validation.py

--- Validation.py
+++ Validation.py
@@ -1,8 +1,10 @@
-import os
-from flask import Flask, request, render_template
-from flask_mysqldb import MySQL
+import dash
+from dash import html, dcc, Input, Output, State
+import dash_bootstrap_components as dbc
+import flask_mysqldb
 
-app = Flask(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 # Configure MySQL
 app.config['MYSQL_HOST'] = 'your_mysql_host'
@@ -10,25 +12,52 @@ app.config['MYSQL_USER'] = 'your_mysql_username'
 app.config['MYSQL_PASSWORD'] = 'your_mysql_password'
 app.config['MYSQL_DB'] = 'your_mysql_database'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-mysql = MySQL(app)
+mysql = flask_mysqldb.MySQL(app)
 
-@app.route('/')
-def home():
-    return render_template('login.html')
 
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
+def login_layout():
+    return html.Div([
+        html.H1("Login"),
+        dbc.Input(id="username-input", type="text", placeholder="Username", style={'margin-bottom': '10px'}),
+        dbc.Input(id="password-input", type="password", placeholder="Password", style={'margin-bottom': '10px'}),
+        dbc.Button("Login", id="login-button", color="primary", n_clicks=0)
+    ], style={'text-align': 'center', 'margin': '100px auto', 'width': '50%'})
 
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users WHERE username = %s", (username,))
-    user = cur.fetchone()
 
-    if user and user['password'] == password:
-        return "Login successful"
+@app.callback(
+    Output('url', 'pathname'),
+    Input('login-button', 'n_clicks'),
+    State('username-input', 'value'),
+    State('password-input', 'value')
+)
+def authenticate(n_clicks, username, password):
+    if n_clicks > 0:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+        user = cur.fetchone()
+        if user and user['password'] == password:
+            return "/success"
+        else:
+            return "/invalid"
+
+
+@app.callback(
+    Output('page-content', 'children'),
+    Input('url', 'pathname')
+)
+def display_page(pathname):
+    if pathname == '/success':
+        return html.H1("Login Successful!")
+    elif pathname == '/invalid':
+        return html.H1("Invalid Username or Password")
     else:
-        return "Invalid username or password"
+        return login_layout()
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
 
 if __name__ == '__main__':
-    app.run()
+    app.run_server(debug=True)
